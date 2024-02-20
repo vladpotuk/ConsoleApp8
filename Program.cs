f03e12118186ecbe3ce6d917aa00c752084c1b2a
vladpotuk <vladpotuk01@gmail.com>
2024-02-20 20:03:04 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введіть число: ");
+        double number = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введіть відсоток: ");
+        double percent = Convert.ToDouble(Console.ReadLine());
+
+        double result = (percent / 100) * number;
+
+        Console.WriteLine($"Результат: {percent}% від {number} = {result}");
+    }
+}
+
